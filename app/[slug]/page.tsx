3c9c5f37923b2b1528338001721032709c3999a8
Alan Williams <alan.williams@vercel.com>
2023-03-31 10:48:08 +0100

    test

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -5,17 +5,24 @@ export const revalidate = 0;
 const fetchBlogs = async () => {
   return await fetch("https://639040c665ff4183110d7bdd.mockapi.io/blogs", {
     headers: {
-      Authorization: 'Bearer savcwv234v2342qvq3245q32456b3q46n3q4b6qb'
-    }
+      Authorization: "Bearer savcwv234v2342qvq3245q32456b3q46n3q4b6qb",
+    },
   });
 };
 
+const fetchBlog = async (id: string) => {
+  return await fetch(`https://639040c665ff4183110d7bdd.mockapi.io/blogs/${id}`);
+};
+
 export async function generateStaticParams() {
-  const posts = await fetchBlogs();
-  return [{ slug: "post-1" }];
+  const blogs = await (await fetchBlogs()).json();
+  return blogs.map((blog: { title: string }, ind: number) => ({
+    slug: blog.title,
+  }));
 }
 
 export default async function Page({ params }: { params: { slug: string } }) {
-  if (!params.slug.match("post-1")) notFound();
+  const blogData = await fetchBlog("5");
+  if (blogData.status === 500) notFound();
   return <main>SLUG: {params.slug}</main>;
 }
