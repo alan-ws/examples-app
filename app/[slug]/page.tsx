a2369e3ba37013bffeb3a3afc70a937fac52e47a
Alan Williams <alan.williams@vercel.com>
2023-03-31 09:19:39 +0100

    does it break

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -6,9 +6,14 @@ const fetchBlogs = async () => {
   return await fetch("https://639040c665ff4183110d7bdd.mockapi.io/blogs");
 };
 
-export default async function Page({ params }: { params: { slug: string } }) {
-  const blogs = await fetchBlogs();
+export async function generateStaticParams() {
+  const posts = await fetchBlogs();
 
-  if (blogs.ok) notFound();
-  return <main>SLUG</main>;
+  if (posts.ok) notFound();
+
+  return [{ slug: "post-1" }];
+}
+
+export default async function Page({ params }: { params: { slug: string } }) {
+  return <main>SLUG: {params.slug}</main>;
 }
