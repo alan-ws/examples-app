423a2d09814b8b0dfe3d0e8eddf992d6517ea781
Alan Williams <alan.williams@vercel.com>
2023-03-31 09:04:17 +0100

    test

---
+++ app/[slug]/page.tsx
@@ -0,0 +1,12 @@
+import { notFound } from "next/navigation";
+
+const fetchBlogs = async () => {
+  return await fetch("https://639040c665ff4183110d7bdd.mockapi.io/blogs");
+};
+
+export default async function Page({ params }: { params: { slug: string } }) {
+  const blogs = await fetchBlogs();
+
+  if (blogs.ok) notFound();
+  return <main>SLUG</main>;
+}
