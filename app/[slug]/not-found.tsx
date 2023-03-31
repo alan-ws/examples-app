1c294c4a698826be90de7e59e561c7cae96f809d
Alan Williams <alan.williams@vercel.com>
2023-03-31 15:01:40 +0100

    test

--- app/[slug]/not-found.tsx
+++ app/[slug]/not-found.tsx
@@ -1,8 +1,18 @@
-export default function NotFound() {
+import Image from "next/image";
+
+const fetchBrandedStuff = async () => {
+  return await (
+    await fetch("https://639040c665ff4183110d7bdd.mockapi.io/brand/1")
+  ).json();
+};
+
+export default async function NotFound() {
+  const res = await fetchBrandedStuff();
   return (
     <>
       <h2>Not Found</h2>
       <p>Could not find requested resource</p>
+      <Image src={res.img} height={480} width={640} alt="animal" />
     </>
   );
 }
