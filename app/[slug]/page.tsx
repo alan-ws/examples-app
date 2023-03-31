5a8ebc60da112994b65094ef931385f132705021
Alan Williams <alan.williams@vercel.com>
2023-03-31 14:55:46 +0100

    test

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,17 +1,25 @@
 import { notFound } from "next/navigation";
 
-export const revalidate = 0;
-
 const fetchBlogs = async () => {
   return await fetch("https://639040c665ff4183110d7bdd.mockapi.io/blogs", {
     headers: {
       Authorization: "Bearer savcwv234v2342qvq3245q32456b3q46n3q4b6qb",
     },
+    next: {
+      revalidate: 0,
+    },
   });
 };
 
 const fetchBlog = async (id: string) => {
-  return await fetch(`https://639040c665ff4183110d7bdd.mockapi.io/blogs/${id}`);
+  return await fetch(
+    `https://639040c665ff4183110d7bdd.mockapi.io/blogs/${id}`,
+    {
+      next: {
+        revalidate: 0,
+      },
+    }
+  );
 };
 
 export async function generateStaticParams() {
